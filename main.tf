8f31888a4015e78b768568adc851ff411a692667
Jesper Struck <jesper.struck@qbeyond.de>
2023-07-06 11:56:10 +0000

    changes, ip groups as parameter

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_firewall_policy_rule_collection_group" "this" {
-  name               = "rcg-${local.responsibility}"
+  name               = "rcg-${var.responsibility}"
   firewall_policy_id = var.firewall_policy_id
   priority           = 100
 
@@ -11,8 +11,8 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     rule {
       name                  = "allow-alz-to-dc-inbound"
       protocols             = ["TCP", "UDP"]
-      source_ip_groups      = [azurerm_ip_group.ipg_aplication_lz.id]
-      destination_ip_groups = [azurerm_ip_group.ipg_azure_dc.id, azurerm_ip_group.ipg_onprem_dc.id]
+      source_ip_groups      = [var.ipg_aplication_lz_id]
+      destination_ip_groups = [var.ipg_azure_dc_id, var.ipg_onpremise_dc_id]
       destination_ports = [
         "53", "88", "123", "135", "137", "138", "139",
         "389", "445", "464", "636", "3268", "3269", "9389"
@@ -28,8 +28,8 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     rule {
       name                  = "allow-dnsresolver-to-dc-inbound"
       protocols             = ["Any"]
-      source_ip_groups      = [azurerm_ip_group.ipg_azure_dc.id]
-      destination_ip_groups = [azurerm_ip_group.ipg_DNSPrivateResolver.id]
+      source_ip_groups      = [var.ipg_azure_dc_id]
+      destination_ip_groups = [var.ipg_dnsprivateresolver_id]
       destination_ports     = ["*"]
     }
   }
@@ -42,7 +42,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     rule {
       name              = "allow-alz-to-kms-outbound"
       protocols         = ["TCP"]
-      source_ip_groups  = [azurerm_ip_group.ipg_aplication_lz.id]
+      source_ip_groups  = [var.ipg_aplication_lz_id]
       destination_fqdns = ["kms.core.windows.net", "azkms.core.windows.net"]
       destination_ports = ["1688"]
     }
@@ -58,7 +58,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     rule {
       name             = "allow-backup-monitoring-outbound"
       protocols        = ["TCP"]
-      source_ip_groups = [azurerm_ip_group.ipg_aplication_lz.id]
+      source_ip_groups = [var.ipg_aplication_lz_id]
       destination_addresses = ["AzureBackup",
         "AzureMonitor",
         "AzureActiveDirectory",
@@ -71,7 +71,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
     rule {
       name                  = "allow-update-management-outbound"
       protocols             = ["TCP"]
-      source_ip_groups      = [azurerm_ip_group.ipg_aplication_lz.id]
+      source_ip_groups      = [var.ipg_aplication_lz_id]
       destination_addresses = ["AzureUpdateDelivery", "AzureFrontDoor.FirstParty"]
       destination_ports     = ["80", "443"]
     }
