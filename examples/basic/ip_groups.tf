0583abc5c2465a51df3634f9d5f8b4165a7cf3f9
Jesper Struck <jesper.struck@qbeyond.de>
2023-07-06 11:53:17 +0000

    changes, ip groups as parameter

---
+++ examples/basic/ip_groups.tf
@@ -0,0 +1,47 @@
+resource "azurerm_ip_group" "azure_dc" {
+  name                = "ipg-azure-dcs"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  cidrs = ["10.0.0.10/32", "10.0.0.11/32"]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_ip_group" "onpremise_dc" {
+  name                = "ipg-onprem-dcs"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  cidrs = []
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_ip_group" "dnsprivateresolver" {
+  name                = "ipg-DNSPrivateResolver"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  cidrs = ["10.0.1.0/24"]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_ip_group" "aplication_lz" {
+  name                = "ipg-application-landing-zone"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  cidrs = ["10.0.2.0/24"]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
