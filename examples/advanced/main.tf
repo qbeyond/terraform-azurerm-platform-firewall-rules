ee4aebb07c9234bd64813457addac6cd1ad753ac
Christian Hartmann <christian.hartmann@qbeyond.de>
2024-04-09 13:33:04 +0200

    removed unnecessary resources from examples

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -8,28 +8,6 @@ resource "azurerm_resource_group" "example" {
   location = local.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-example"
-  address_space       = ["10.0.0.0/16"]
-  location            = local.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "AzureFirewallSubnet" # Must be exact 'AzureFirewallSubnet'
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.0.0/16"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "pip-example"
-  location            = local.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
 resource "azurerm_firewall_policy" "example" {
   name                = "fwp-example"
   resource_group_name = azurerm_resource_group.example.name
