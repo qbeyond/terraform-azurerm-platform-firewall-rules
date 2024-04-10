03c1063219df3c4ddd91edd700e3e49a423a7821
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2024-04-10 10:29:27 +0200

    Allow DNS over TCP
    Co-authored-by: Thimo Limpert <104893472+QBY-ThimoLimpert@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "this" {
 
       rule {
         name                  = "allow-dc-to-dnsresolver-inbound"
-        protocols             = ["UDP"]
+        protocols             = ["UDP","TCP"]
         source_ip_groups      = var.ipg_onpremise_dc_id != null ? [var.ipg_azure_dc_id, var.ipg_onpremise_dc_id] : [var.ipg_azure_dc_id]
         destination_ip_groups = [var.ipg_dnsprivateresolver_id]
         destination_ports     = ["53"]
